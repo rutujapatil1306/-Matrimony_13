6cdc096c1a0cff61b34d8d3eeda0502858b60e73
Somesh Bandal <someshbandal1312@gmail.com>
2025-11-24 12:42:52 +0530

    added profile section

--- src/main/java/com/spring/jwt/profile/ProfileRepository.java
+++ src/main/java/com/spring/jwt/profile/ProfileRepository.java
@@ -4,6 +4,10 @@ import com.spring.jwt.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<UserProfile, Integer> {
+    Optional<UserProfile> findByUserId(Integer userId);
+
 }
--- src/main/java/com/spring/jwt/profile/ProfileService.java
+++ src/main/java/com/spring/jwt/profile/ProfileService.java
@@ -2,4 +2,5 @@ package com.spring.jwt.profile;
 
 public interface ProfileService {
 
+    ProfileDTO createProfile(Integer userId, ProfileDTO profileDTO);
 }
